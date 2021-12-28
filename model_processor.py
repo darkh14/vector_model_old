72eda7c49685992b3ce109aaf76679a2d4c03474
Ziuz Pavel <dark_h14@mail.ru>
2021-12-28 22:47:26 +0300

    add model predict (add graph)

--- model_processor.py
+++ model_processor.py
@@ -32,16 +32,6 @@ class ModelProcessor:
 
         self.model = None
 
-        # self.X = np.array([[]])
-        # self.X_scaled = np.array([[]])
-        # self.y = np.array([])
-        # self.X_pred = np.array([[]])
-        # self.X_pd_pred = pd.DataFrame(np.array([[]]))
-        # self.y_pd_pred = pd.DataFrame(np.array([[]]))
-        # self.model = None
-        # self._model_is_set = False
-        # self.graph_file_name = 'graph.png'
-
     def load_data(self, parameters):
 
         raw_data = parameters.get('data')
@@ -59,70 +49,42 @@ class ModelProcessor:
         if not model_description:
             raise ProcessorException('model is not in parameters')
 
+        need_to_update = bool(parameters.get('need_to_update'))
+
         self.model = Model(model_description['id'],
                            model_description['name'],
                            model_description['x_indicators'],
-                           model_description['y_indicators'])
+                           model_description['y_indicators'],
+                           need_to_update=need_to_update)
 
-        self.model.fit(parameters.get('epochs'), parameters.get('validation_split'))
+        history = self.model.fit(parameters.get('epochs'), parameters.get('validation_split'))
 
-    #
+        return history
 
-    #
-    # def _set_model(self, model_name='', renew=False, do_not_create=False):
-    #
+    def predict(self, parameters):
 
-    #
-    # def _save_model(self):
-    #
+        model_description = parameters.get('model')
 
-    #
-    # @staticmethod
-    # def _create_model(inputs_number, outputs_number):
+        if not model_description:
+            raise ProcessorException('model is not in parameters')
 
-    #
-    # @staticmethod
-    # def _make_x_y_list(x_list, y_list):
-    #     x_set = set(x_list)
-    #     y_set = set(y_list)
-    #     return list(x_set.union(y_set))
-    #
+        self.model = Model(model_description['id'])
 
-    #
-    # def _make_graph(self, x, y, x_label, y_label):
-    #
-    #     fig, ax = plt.subplots()
-    #
-    #     ax.plot(x, y)# , label='y_test')
-    #
-    #     ax.set_xlabel(x_label)
-    #     ax.set_ylabel(y_label)
-    #     # ax.legend()
-    #
-    #     # plt.show()
-    #     fig.savefig(self.graph_file_name)
-    #
-    # def read_graph_file(self):
-    #
-    #     f = open(self.graph_file_name, 'rb')
-    #     result = f.read()
-    #     f.close()
-    #
-    #     return result
-    #
-    # @staticmethod
-    # def _get_dataframe_for_graph(x, x_indicator):
-    #     columns = [x_indicator + '_' + str(number) for number in range(1, 13)]
-    #     result = x[columns]
-    #     result = result.sum(axis=1)
-    #
-    #     result = result/12
-    #     return np.array(result)
+        inputs = parameters.get('inputs')
+        if not inputs:
+            raise ProcessorException('inputs not found in parameters')
+
+        get_graph = parameters.get('get_graph')
+        graph_data = parameters.get('graph_data')
+
+        prediction, y_columns, graph_bin = self.model.predict(inputs, get_graph=get_graph, graph_data=graph_data)
+
+        return prediction, y_columns, graph_bin
 
 
 class Model:
 
-    def __init__(self, model_id, name='', x_indicators=[], y_indicators=[], need_to_update=False):
+    def __init__(self, model_id, name='', x_indicators=None, y_indicators=None, need_to_update=False):
         self.model_id = model_id
         self._db_connector = DB_CONNECTOR
         self._data_processor = DataProcessor()
@@ -139,15 +101,22 @@ class Model:
             self.y_columns = description_from_db['y_columns']
         else:
             self.name = name
-            self.x_indicators = self._data_processor.get_indicator_ids(x_indicators)
-            self.y_indicators = self._data_processor.get_indicator_ids(y_indicators)
+            self.x_indicators = []
+            self.y_indicators = []
             self.periods = []
             self.organisations = []
             self.scenarios = []
             self.x_columns = []
             self.y_columns = []
 
+        if x_indicators:
+            self.x_indicators = self._data_processor.get_indicator_ids(x_indicators)
+
+        if y_indicators:
+            self.y_indicators = self._data_processor.get_indicator_ids(y_indicators)
+
         self.need_to_update = not description_from_db or need_to_update
+        self.graph_file_name = 'graph.png'
 
     def update_model(self, inputs, outputs):
         if self.need_to_update:
@@ -185,6 +154,8 @@ class Model:
         X, y, x_columns, y_columns = self._data_processor.get_X_y_for_fitting(inputs, outputs, additional_data)
         self.x_columns = x_columns
         self.y_columns = y_columns
+        if not retrofit:
+            self._data_processor.write_columns(self.model_id, x_columns, y_columns)
 
         scaler = self._get_scaler(retrofit=retrofit)
         X = scaler.fit_transform(X)
@@ -202,23 +173,36 @@ class Model:
         return history.history
 
     def predict(self, inputs, indicators=None, get_graph=False, graph_data=None):
-        self.inputs_pred = inputs
-        self.prepare_data(for_prediction=True)
-        self.read_columns()
-        self._set_model()
-        y = self.model.predict(self.X_pred)
-        self.y_pd_pred = pd.DataFrame(y, columns=self._columns['y_columns'])
+
+        additional_data = {'x_indicators': self.x_indicators,
+                           'y_indicators': self.y_indicators,
+                           'periods': self.periods,
+                           'organisations': self.organisations,
+                           'scenarios': self.scenarios,
+                           'x_columns': self.x_columns,
+                           'y_columns': self.y_columns}
+
+        X = self._data_processor.get_X_for_prediction(inputs, additional_data)
+
+        scaler = self._get_scaler(True)
+        X = scaler.fit_transform(X)
+
+        inner_model = self._get_inner_model(retrofit=True)
+        y = inner_model.predict(X)
+
+        y_pd = pd.DataFrame(y, columns=self.y_columns)
 
         graph_bin = None
         if get_graph:
-            x_graph = self._get_dataframe_for_graph(self.X_pd_pred, graph_data['x_indicator'])
-            y_graph = self._get_dataframe_for_graph(self.y_pd_pred, graph_data['y_indicator'])
+            X_pd = pd.DataFrame(X, columns=self.x_columns)
+            x_graph = self._get_dataframe_for_graph(X_pd, graph_data['x_indicator'])
+            y_graph = self._get_dataframe_for_graph(y_pd, graph_data['y_indicator'])
 
             self._make_graph(x_graph, y_graph, graph_data['x_indicator'], graph_data['y_indicator'])
 
             graph_bin = self.read_graph_file()
 
-        return self.y_pd_pred.to_dict('records'), graph_bin
+        return y_pd.to_dict('records'), self.y_columns, graph_bin
 
     def _get_scaler(self, retrofit=False):
 
@@ -231,22 +215,10 @@ class Model:
 
         return scaler
 
-    def _get_indicator_id(self, indicator, report_type):
-
-        indicator_from_db = self._db_connector.read_indicator_from_name_type(indicator, report_type)
-
-        if not indicator_from_db:
-            indicator_id = 'ind_' + settings_controller.get_id()
-            self._db_connector.write_indicator(indicator_id, indicator, report_type)
-        else:
-            indicator_id = indicator_from_db['indicator_id']
-
-        return indicator_id
-
-    def _get_inner_model(self, inputs_number, outputs_number, retrofit=False):
+    def _get_inner_model(self, inputs_number=0, outputs_number=0, retrofit=False):
 
         if retrofit:
-            inner_model = self._data_processor.read_inner_model()
+            inner_model = self._data_processor.read_inner_model(self.model_id)
             if not inner_model:
                 inner_model = self._create_inner_model(inputs_number, outputs_number)
         else:
@@ -254,26 +226,8 @@ class Model:
 
         return inner_model
 
-
-        # folder_name = "model"
-        #     if renew or not self._model_is_set:
-        #         create = False
-        #
-        #         if not renew:
-        #             if os.path.isdir(folder_name):
-        #                 self.model = self.model =
-        #             elif not do_not_create:
-        #                 create = True
-        #         else:
-        #             create = True
-        #
-        #         if create:
-        #             self.model = self._create_model(self.X.shape[1], self.y.shape[1])
-
-        #     folder_name = "model"
-        #     self.model.save(folder_name)
-
-    def _create_inner_model(self, inputs_number, outputs_number):
+    @staticmethod
+    def _create_inner_model(inputs_number, outputs_number):
         model = Sequential()
         model.add(Dense(500, activation="relu", input_shape=(inputs_number,), name='dense_1'))
         model.add(Dense(250, activation="relu", name='dense_2'))
@@ -281,6 +235,38 @@ class Model:
         model.add(Dense(outputs_number, activation="linear", name='dense_4'))
 
         return model
+
+    def _make_graph(self, x, y, x_label, y_label):
+
+        fig, ax = plt.subplots()
+
+        ax.plot(x, y) # , label='y_test')
+
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+        # ax.legend()
+
+        # plt.show()
+        fig.savefig(self.graph_file_name)
+
+    def read_graph_file(self):
+
+        f = open(self.graph_file_name, 'rb')
+        result = f.read()
+        f.close()
+
+        return result
+
+    def _get_dataframe_for_graph(self, x, x_indicator):
+
+        indicator_description = self._data_processor.get_indicator_from_name_type(x_indicator['indicator'],
+                                                                                  x_indicator['report_type'])
+        columns = [indicator_description['indicator_id'] + '_' + str(number) for number in range(1, 13)]
+        result = x[columns]
+        result = result.sum(axis=1)
+
+        result = result/12
+        return np.array(result)
 
 
 class DataProcessor:
@@ -304,6 +290,10 @@ class DataProcessor:
         return self._db_connector.read_model_description(model_id)
 
     def get_indicator_ids(self, indicators):
+
+        if not indicators:
+            return []
+
         result = [self._db_connector.read_indicator_from_name_type(ind_line['indicator'],
                                                                    ind_line['report_type'])['indicator_id']
                   for ind_line in indicators]
@@ -353,6 +343,22 @@ class DataProcessor:
         y = output_data_col.to_numpy()
 
         return X, y, x_columns, y_columns
+
+    def get_X_for_prediction(self, inputs, additional_data):
+
+        inputs = self.add_indicators_to_raw_data(inputs)
+        input_data_col = self._prepare_dataset_merge(inputs, additional_data['x_indicators'])
+
+        additional_data['years'] = list(set([self._get_year(period) for period in additional_data['periods']]))
+        input_data_col = input_data_col.fillna(0)
+
+        input_data_col = self._prepare_dataset_sort_one_hot(input_data_col, additional_data)
+
+        input_data_col.drop(['organisation', 'scenario', 'year'], axis=1, inplace=True)
+
+        X = input_data_col.to_numpy()
+
+        return X
 
     def write_columns(self, model_id, x_columns, y_columns):
         self._db_connector.write_model_columns(model_id, x_columns, y_columns)
@@ -406,7 +412,10 @@ class DataProcessor:
         pd_dataset = pd.DataFrame(dataset)
         pd_dataset = self._add_month_year_to_data(pd_dataset)
 
-        pd_dataset.drop(['_id', 'indicator', 'report_type'], axis=1, inplace=True)
+        columns_to_drop = ['indicator', 'report_type']
+        if '_id' in list(pd_dataset.columns):
+            columns_to_drop.append('_id')
+        pd_dataset.drop(columns_to_drop, axis=1, inplace=True)
         pd_dataset.rename({'indicator_id': 'indicator'}, axis=1, inplace=True)
 
         dataset_col, raw_dataset_grouped = self._group_dataset(pd_dataset)
@@ -524,6 +533,18 @@ class DataProcessor:
                            os.path.relpath(os.path.join(c_dir, file),
                                            os.path.join(path, '..')))
 
+    def _get_indicator_id(self, indicator, report_type):
+
+        indicator_from_db = self._db_connector.read_indicator_from_name_type(indicator, report_type)
+
+        if not indicator_from_db:
+            indicator_id = 'ind_' + settings_controller.get_id()
+            self._db_connector.write_indicator(indicator_id, indicator, report_type)
+        else:
+            indicator_id = indicator_from_db['indicator_id']
+
+        return indicator_id
+
 
 def load_data(parameters):
 
@@ -541,14 +562,9 @@ def fit(parameters):
 
 
 def predict(parameters):
-    inputs = parameters.get('inputs')
-    if not inputs:
-        raise ProcessorException('inputs not found in parameters')
 
-    get_graph = parameters.get('get_graph')
-    graph_data = parameters.get('graph_data')
     processor = ModelProcessor(parameters)
-    prediction, graph_bin = processor.predict(inputs, get_graph=get_graph, graph_data=graph_data)
+    prediction, y_columns, graph_bin = processor.predict(parameters)
 
     result = dict(status='OK', error_text='', result=prediction, description='model data loaded')
 
