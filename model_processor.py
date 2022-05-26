d2be8c4297fd2d23159f0c88dba3b7287df9340b
Ziuz Pavel <dark_h14@mail.ru>
2022-05-26 15:45:34 +0300

    fix tensorflow library call

--- model_processor.py
+++ model_processor.py
@@ -14,13 +14,13 @@ from job_processor import JobProcessor
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
-from keras.wrappers.scikit_learn import KerasRegressor
+from tensorflow.python.keras.wrappers.scikit_learn import KerasRegressor
 import eli5
 import tensorflow as tf
 from eli5.sklearn import PermutationImportance
-from tensorflow.keras.models import Sequential, clone_model
-from tensorflow.keras.layers import Dense
-from tensorflow.keras import optimizers
+from tensorflow.python.keras.models import Sequential, clone_model
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras import optimizers
 
 from tensorflow import keras
 
